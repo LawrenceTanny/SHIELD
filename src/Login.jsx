ca29261428b9d6c5529acb2180aeb04cb781d00b
LawrenceTanny <lawrencetan1104@gmail.com>
2026-03-30 21:30:16 +0800

    Setup API calls to the backend/free very slow haha :p

--- src/Login.jsx
+++ src/Login.jsx
@@ -33,7 +33,7 @@ export default function Login({ onClose }) {
 
       setIsLoading(true);
       try {
-        const response = await fetch("http://localhost:5000/api/signup", {
+        const response = await fetch("https://shield-mfoi.onrender.com/api/signup", {
           method: "POST",
           headers: { "Content-Type": "application/json" },
           body: JSON.stringify({
@@ -64,7 +64,7 @@ export default function Login({ onClose }) {
 
       setIsLoading(true);
       try {
-        const response = await fetch("http://localhost:5000/api/login", {
+        const response = await fetch("https://shield-mfoi.onrender.com/api/login", {
           method: "POST",
           headers: { "Content-Type": "application/json" },
           body: JSON.stringify({
