f2b323d33c2623197acaf41302b698c5f1d82c2f
LawrenceTanny <lawrencetan1104@gmail.com>
2026-04-01 23:10:14 +0800

    Fixed Footer

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -3,7 +3,7 @@ import "./Styles/Footer.css";
 export default function Footer() {
   return (
     <footer className="app-footer">
-      <p>Footer Placeholder — you can customize this later.</p>
+      <p>Bulacan State University 2026.</p>
     </footer>
   );
 }
